2394ebb8591be3d8d6cf58c4f25ea764b5780ae8
Bianca França <biiafranca@hotmail.com>
2025-05-10 13:16:10 -0300

    refactor: include BRA vaccine data and change to use people_vaccinated instead of total_vaccinations

--- etl/generate_csv_data.py
+++ etl/generate_csv_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 # ===================== MAIN DATA =====================
 covid_data = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
 df = pd.read_csv(covid_data)
-df = df[['iso_code', 'location', 'date', 'total_cases', 'total_deaths', 'total_vaccinations']]
+df = df[['iso_code', 'location', 'date', 'total_cases', 'total_deaths', 'people_vaccinated']]
 df = df[df['iso_code'].str.len() == 3]  # Filter valid ISO country codes
 
 # Assign unique IDs to each CovidCase and VaccinationStats entry (based on country/date rows)
@@ -35,12 +35,12 @@ covid_cases.to_csv("etl/data/covid_cases.csv", index=False)
 print(f"Saving covid_cases.csv with {len(covid_cases)} rows...")
 
 # ===================== NODE: VaccinationStats =====================
-df_vacc = df.dropna(subset=['iso_code', 'date', 'total_vaccinations'], how='any').copy()
-vacc_stats = df_vacc[['vaccstats_id', 'iso_code', 'date', 'total_vaccinations']].copy()
+df_vacc = df.dropna(subset=['iso_code', 'date', 'people_vaccinated'], how='any').copy()
+vacc_stats = df_vacc[['vaccstats_id', 'iso_code', 'date', 'people_vaccinated']].copy()
 vacc_stats.rename(columns={
     'vaccstats_id': 'id',
     'iso_code': 'country_iso',
-    'total_vaccinations': 'totalVaccinated'
+    'people_vaccinated': 'totalVaccinated'
 }, inplace=True)
 vacc_stats['totalVaccinated'] = vacc_stats['totalVaccinated'].astype(int)
 vacc_stats.to_csv("etl/data/vaccination_stats.csv", index=False)
@@ -64,6 +64,17 @@ print(f"Saving vaccinated_on.csv with {len(vaccinated_on)} rows...")
 vac_manuf_url = "https://covid.ourworldindata.org/data/vaccinations/vaccinations-by-manufacturer.csv"
 df_vac_by_manuf = pd.read_csv(vac_manuf_url)
 
+vac_br = "https://covid.ourworldindata.org/data/vaccinations/country_data/Brazil.csv"
+df_vac_br = pd.read_csv(vac_br)
+
+# Set and unify Brazil data with Global data:
+df_vac_br = df_vac_br[['date', 'vaccine']].dropna()
+df_vac_br['vaccine'] = df_vac_br['vaccine'].str.split(', ')
+df_vac_br = df_vac_br.explode('vaccine')
+df_vac_br['location'] = 'Brazil'
+df_vac_br = df_vac_br[['location', 'vaccine', 'date']]
+df_vac_by_manuf = pd.concat([df_vac_by_manuf, df_vac_br], ignore_index=True)
+
 # ===================== NODE: Vaccine =====================
 vaccines = df_vac_by_manuf.groupby('vaccine')['date'].min().reset_index().copy()
 vaccines['id'] = range(1, len(vaccines) + 1)
@@ -76,10 +87,18 @@ print(f"Saving vaccines.csv with {len(vaccines)} rows...")
 country_iso_map = countries.set_index('name')['iso3'].to_dict()
 uses_raw = df_vac_by_manuf[['location', 'vaccine', 'date']].copy()
 uses_raw['country_iso'] = uses_raw['location'].map(country_iso_map)
-uses_raw = uses_raw.dropna(subset=['country_iso'])
+
+total_uses = len(uses_raw)
+uses_raw_valid = uses_raw.dropna(subset=['country_iso'])
+missing_data = total_uses - len(uses_raw_valid)
+
+if missing_data > 0:
+    missing_countries = uses_raw[uses_raw['country_iso'].isna()]['location'].unique()
+    print("WARNING! Entries were ignored due to country matching failure")
+    print(f"Ignored countries: {sorted(missing_countries)}")
 
 # Keep only the first use per country-vaccine pair
-uses = uses_raw.groupby(['country_iso', 'vaccine'])['date'].min().reset_index()
+uses = uses_raw_valid.groupby(['country_iso', 'vaccine'])['date'].min().reset_index()
 uses.rename(columns={'date': 'first_used'}, inplace=True)
 
 uses.to_csv("etl/data/uses.csv", index=False)
