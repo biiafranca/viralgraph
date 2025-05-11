c25ca6486c3ca3bf9335cc13eaecc234f0428c5f
Bianca França <biiafranca@hotmail.com>
2025-05-11 17:10:50 -0300

    refactor: adjust to use chi router

--- api/handlers/vaccination/controller.go
+++ api/handlers/vaccination/controller.go
@@ -3,23 +3,13 @@ package vaccination
 import (
 	"net/http"
 	"strings"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func VaccinationController(w http.ResponseWriter, r *http.Request) {
-	path := strings.TrimPrefix(r.URL.Path, "/vaccination/")
-	parts := strings.Split(path, "/")
-
-	var country, date string
-	if len(parts) == 1 {
-		date = parts[0]
-	} else if len(parts) == 2 {
-		country = parts[0]
-		date = parts[1]
-	} else {
-		http.NotFound(w, r)
-		return
-	}
-
+	country := chi.URLParam(r, "country")
+	date := chi.URLParam(r, "date")
 	onlyNews := strings.ToLower(r.URL.Query().Get("only-news")) == "true"
 
 	if onlyNews {
